21146a8c5a457db6a1add8a3770cc44f96b56a03
Will King <contact@wking.io>
2018-01-10 10:02:43 -0600

    WIP

--- themes/confero/page-biography.php
+++ themes/confero/page-biography.php
@@ -24,8 +24,8 @@ $nums = array(
 
 $i = 1;
 
-if ( have_rows('hero_slides_desktop') ) :
-  while ( have_rows('hero_slides_desktop') ) : the_row();
+if ( have_rows('confero_steps') ) :
+  while ( have_rows('confero_steps') ) : the_row();
     $steps[$i]['num'] = $nums[$i];
     $steps[$i]['title'] = get_sub_field('step_title'); 
     $steps[$i]['content'] = get_sub_field('step_content');
@@ -33,23 +33,27 @@ if ( have_rows('hero_slides_desktop') ) :
 	endwhile;
 endif;
 
+error_log( print_r( $steps, true ) );
+
 ?>
 
   <main id="main" class="main" role="main">
     <section class="biography">
-      <div class="container">
+      <div class="container flex items-start">
         <div class="biography__info">
           <h2 class="biography__title"><?php echo $bio_title ?></h2>
-          <div><?php echo $bio_content; ?></div>
+          <div class="biography__content"><?php echo $bio_content; ?></div>
           <button class="biography__btn btn btn--ghost">See Christopher Confero In Action</button>
         </div>
-        <img src="<?php echo $bio_img['url'] ?>" alt="" class="biography__img">
+        <div class="biography__headshot">
+          <img src="<?php echo $bio_img['url'] ?>" alt="" class="biography__headshot__img">
+        </div>
       </div>
     </section>
 
-    <section class="promo-video" <?php if ($promo_bg) { echo 'style="background-image: url(' . $promo_bg . ')" '; }?>>
+    <section class="promo-video" <?php if ($promo_bg) { echo 'style="background-image: url(' . $promo_bg['url'] . ')" '; }?>>
       <div class="promo-video__overlay"></div>
-      <div class="container">
+      <div class="container flex">
         <div class="promo-video__play">
           <button class="promo-video__play__btn">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 82">
@@ -74,14 +78,21 @@ endif;
 
     <?php if (! empty( $steps ) ) : ?>
       <section class="steps">
-        <div class="container">
-          <?php if ( ! empty( $steps['num'] ) ) : ?>
-            <ul class="steps__nav">
-              <?php foreach ( $steps as $step ) : ?>
-                <li class="steps__nav__item"><?php echo 'Step ' . $step['num']; ?></li>
-              <?php endforeach; ?>
-            </ul>
-          <?php endif; ?>
+        <div class="container steps-slider">
+          <ul class="steps__nav flex" data-active="1">
+            <?php foreach ( $steps as $i => $step ) : ?>
+              <li class="steps__nav__item steps__nav__item--desktop"><?php echo 'Step ' . $step['num']; ?></li>
+              <li class="steps__nav__item steps__nav__item--mobile"><?php echo '0' . $i; ?></li>
+            <?php endforeach; ?>
+          </ul>
+          <div class="slider">
+            <?php foreach ( $steps as $step ) : ?>
+              <div class="steps-slider__content">
+                <h3 class="steps-slider__title"><?php echo $step['title']; ?></h3>
+                <div class="steps-slider__content"><?php echo $step['content']; ?></div>
+              </div>
+            <?php endforeach; ?>
+          </div>
         </div>
       </section>
     <?php endif; ?>
--- themes/confero/src/js/sliders.js
+++ themes/confero/src/js/sliders.js
@@ -52,6 +52,52 @@ export function initHeroSlider(context) {
   $(`${context} .slider-next`).click(() => $(`${context} .slider`).slick('slickNext'));
 }
 
+const fancyNav = (context) => {
+  const navItems = Array.from(document.querySelectorAll('.fancy-nav__item')).map((item, index) => {
+    item.addEventListener('click', () => {
+      $(`${context} .slider__items`).slick('slickGoTo', index);
+    });
+
+    return item;
+  });
+
+  // On before slide change
+  /* eslint-disable eqeqeq */
+  $(`${context} .slider__items`).on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+    navItems.map((item) => {
+      item.dataset.sliderState = 'waiting';
+      return true;
+    });
+    navItems[nextSlide].dataset.sliderState = 'current';
+
+    if (nextSlide == 0) {
+      navItems[3].dataset.sliderState = 'prev';
+    } else {
+      navItems[nextSlide - 1].dataset.sliderState = 'prev';
+    }
+
+    if (nextSlide == 3) {
+      navItems[0].dataset.sliderState = 'next';
+    } else {
+      navItems[nextSlide + 1].dataset.sliderState = 'next';
+    }
+  });
+};
+
+export function initStepsSlider(context) {
+  $(`${context} .slider`).slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 7500,
+    adaptiveHeight: true,
+  });
+
+  fancyNav(context);
+}
+
 export function destroySlider(context) {
   $(`${context} .slider`).slick('unslick');
 }
